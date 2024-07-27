1ea7048fa66e353e15ffcd9ff906f37a967656ba
sajika <sjdkhedmati1@gmail.com>
2024-07-27 14:08:40 +0330

    upcoming meeting page

--- app/(application)/(home)/components/meeting-card.tsx
+++ app/(application)/(home)/components/meeting-card.tsx
@@ -2,11 +2,23 @@
 
 import Image from "next/image";
 
-import { Button, cn } from "@nextui-org/react";
+import {
+	Avatar,
+	AvatarGroup,
+	Badge,
+	Button,
+	cn,
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+	Skeleton,
+} from "@nextui-org/react";
 import { toast } from "sonner";
 import { avatarImages } from "@/constants/avatar-images";
 import { MemberResponse } from "@stream-io/video-react-sdk";
 import { useEffect, useState, useTransition } from "react";
+import { getPrimaryEmail } from "../../components/user-list";
+import { formatDistanceToNowStrict } from "date-fns";
 
 interface MeetingCardProps {
 	title: string;
@@ -44,7 +56,7 @@ const MeetingCard = ({
 	}, [members]);
 
 	return (
-		<section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] dark:bg-gray-800 bg-gray-100 px-5 py-8 xl:max-w-[568px]">
+		<section className="flex min-h-[258px] w-full flex-col justify-between gap-4 rounded-[14px] dark:bg-gray-800/60 bg-gray-100 px-5 py-8 xl:max-w-[568px]">
 			<article className="flex flex-col gap-5">
 				<Image
 					src={icon}
@@ -55,42 +67,89 @@ const MeetingCard = ({
 				/>
 				<div className="flex justify-between">
 					<div className="flex flex-col gap-2">
-						<h1 className="text-2xl font-bold">{title}</h1>
-						<p className="text-base font-normal">{date}</p>
+						<h1 className="text-lg md:text-xl font-semibold">{title}</h1>
+						<p className="text-sm font-normal">{date}</p>
 					</div>
 				</div>
 			</article>
-			<article className={cn("flex justify-center relative", {})}>
-				<div className="relative flex w-full max-sm:hidden">
-					{!isPending && memberItems.length > 0 ? (
-						memberItems
-							.slice(0, 3)
-							.map((member, index) => (
-								<Image
-									key={member.user_id}
-									src={member.user.image as string}
-									alt="attendees"
-									width={40}
-									height={40}
-									className={cn("rounded-full", { absolute: index > 0 })}
+			<article
+				className={cn(
+					"flex flex-col md:flex-row gap-y-4 gap-x-2 md:justify-center relative",
+					{},
+				)}
+			>
+				<div className="w-full">
+					{isPending ? (
+						<div className="relative flex w-full">
+							{Array.from(new Array(3)).map((_, index) => (
+								<Skeleton
+									key={index}
+									className={cn("size-10 rounded-full", {
+										absolute: index > 0,
+									})}
 									style={{ top: 0, left: index * 28 }}
 								/>
-							))
-					) : (
-						<p>Public meeting</p>
-					)}
-
-					{!isPending && memberItems.length >= 4 && (
-						<div className="flex-center absolute left-[136px] size-10 rounded-full border-2 bg-gray-200 border-gray-300 dark:border-gray-600 dark:bg-gray-700">
-							+{memberItems.length - 3}
+							))}
 						</div>
+					) : memberItems.length > 0 ? (
+						<AvatarGroup isBordered max={3}>
+							{memberItems.slice(0, 3).map((member, index) => (
+								<Popover
+									showArrow
+									offset={10}
+									placement="top"
+									backdrop="opaque"
+									key={member.user_id}
+								>
+									<PopoverTrigger>
+										<Avatar
+											isBordered
+											color={member.user.online ? "success" : "default"}
+											src={member.user.image as string}
+										/>
+									</PopoverTrigger>
+									<PopoverContent className="px-6 py-3">
+										<div className="flex flex-col gap-4">
+											<div className="flex items-center gap-4">
+												<Avatar src={member.user.image as string} />
+												<aside>
+													<h3>{member.user.name}</h3>
+													<span className="text-sm text-gray-500">
+														{member.user.role}
+													</span>
+												</aside>
+											</div>
+
+											<p className="text-sm">
+												The last activity was{" "}
+												{formatDistanceToNowStrict(
+													member.user.last_active as string,
+												)}{" "}
+												ago
+											</p>
+
+											<p>{}</p>
+										</div>
+									</PopoverContent>
+								</Popover>
+							))}
+						</AvatarGroup>
+					) : (
+						<Button
+							disabled
+							color="success"
+							variant="faded"
+							className="px-4 py-2 text-xs font-semibold w-max rounded-xl"
+						>
+							Public meeting
+						</Button>
 					)}
 				</div>
 				{!isPreviousMeeting && (
-					<div className="flex gap-2">
+					<div className="flex items-center gap-2">
 						<Button
 							color="primary"
-							className="px-6"
+							className="px-6 flex-1 md:flex-initial"
 							startContent={
 								buttonIcon1 && (
 									<Image
